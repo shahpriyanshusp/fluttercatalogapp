24fe95f2020336f16f16b9a7a65265369359a97f
shahpriyanshusp <shahpriyanshu.sp@gmail.com>
2022-09-05 01:59:06 +0530

    day 5 final

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/pages/Login.dart';
 import 'package:flutter_catalog/pages/home_page.dart';
+import 'package:flutter_catalog/utils/routs.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,15 +17,21 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        // primaryTextTheme: GoogleFonts.latoTextTheme(),
+        fontFamily: GoogleFonts.lato().fontFamily
+      ),
       darkTheme: ThemeData(brightness:Brightness.dark ),
       // home: HomePage(),
-      initialRoute: "/home",
+      // initialRoute: "/home",
       routes: {
         "/":(_)=>Login_page(),
-        "/loginpage":(_)=>Login_page(),
-        "/home":(_)=>HomePage()
+        // "/loginpage":(_)=>Login_page(),
+        MyRoutes.loginRoute:(_)=>Login_page(),
+        MyRoutes.homeRoute:(_)=>HomePage()
       },
     );
   }
--- lib/pages/Login.dart
+++ lib/pages/Login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/utils/routs.dart';
 
 class Login_page extends StatefulWidget {
   const Login_page({Key? key}) : super(key: key);
@@ -11,14 +12,48 @@ class _Login_pageState extends State<Login_page> {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center(
-        child: Text(
-          "Login page",
-           style: TextStyle(
-          fontSize: 20,
-      color: Colors.blue,
-        fontWeight: FontWeight.w600
-      ),),),
+      color: Colors.white,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset("assets/images/login_image.png",fit: BoxFit.cover,),
+            SizedBox(height: 20,),
+            Text("Welcome",style: TextStyle(fontSize:24,fontWeight: FontWeight.w600 ),),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 16.0,horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                        hintText:"Enter user name",
+                        labelText: "User Name"
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText:true,
+                    decoration: InputDecoration(
+                        hintText:"Enter Password",
+                        labelText: "Password"
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  ElevatedButton(
+                      onPressed: (){
+                        Navigator.pushNamed(context, MyRoutes.homeRoute);
+                      },
+                      child: Text("Login"),
+                  style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all(Color(0xfffffffff)),
+                    padding: MaterialStateProperty.all(EdgeInsets.symmetric(horizontal: 30))
+                  ),
+                  )
+                ],
+              ),
+            ),
+
+          ],
+        ),
+      )
     );
   }
 }
